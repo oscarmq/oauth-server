2106defb2d9ae9c22b0ce08a8bba09df1a1a809f
Oscar <oscar@onsei.de>
2019-02-10 19:36:35 +0100

    Add handler for google auth

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const app = express();
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret
+        clientSecret: keys.googleClientSecret,
+        callbackURL: 'auth/google/callback'
     }, (accessToken) => {
         console.log(accessToken);
+    })
+);
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
     })
 );
 
