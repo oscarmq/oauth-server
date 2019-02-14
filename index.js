aae440a13251c2a741a889fcbe311464024d5f19
Oscar <oscar@onsei.de>
2019-02-14 22:43:23 +0100

    Add route handler for oauth callback

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('Access token: ', accessToken);
+        console.log('Refresh token: ', refreshToken);
+        console.log('Profile:', profile);
     })
 );
 
@@ -18,6 +20,10 @@ app.get('/auth/google',
     passport.authenticate('google', {
         scope: ['profile', 'email']
     })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google')
 );
 
 const PORT = process.env.PORT || 5000
